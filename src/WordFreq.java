e31cb31ae340965928efe806a80413ac7158c1bf
bvlh <brandonh15527@gmail.com>
2026-02-14 14:56:31 -0800

    first commit

---
+++ src/WordFreq.java
@@ -0,0 +1,8 @@
+public class WordFreq {
+    String word;
+    int frequency;
+    public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
